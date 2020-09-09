29c882822c4d0498253991c0742159b6e3313c4f
Malte <malte.havers@web.de>
2020-09-09 11:09:33 +0200

    Initial sharing of project

---
+++ Telefonbuch.java
@@ -0,0 +1,36 @@
+/**
+*
+*/
+public class Telefonbuch
+{
+  /* Attribute */
+	private BinarySearchTree a;
+	private boolean sortierung;
+  /* Methoden */
+  /**  */
+  public void hinzufuegen(String  eintrag){
+  }
+
+  /**  */
+  public void aendereSortierung(){
+  }
+
+  /**  */
+  public Telefonbucheintrag search(String  eingabe){
+      return null;
+  }
+
+  /**  */
+  public int uebersetzeInIndex(String  eingabe){
+      return 0;
+  }
+
+  /**  */
+  public void löschen(String  eingabe){
+  }
+
+  /**  */
+  public void veraendern(String  eingabe){
+  }
+
+}//Ende Klasse: Telefonbuch
---
+++ Telefonbucheintrag.java
@@ -0,0 +1,39 @@
+/**
+*
+*/
+public class Telefonbucheintrag
+{
+  /* Attribute */
+    private List nummer;
+    private String nachname;
+    private String name;
+  /* Methoden */
+  
+  /**  */
+  public void setzeNummer(String  nummer){
+  }
+
+  /**  */
+  public void setzeNachname(String  nachname){
+  }
+
+  /**  */
+  public void setzeName(String  name){
+  }
+
+  /**  */
+  public String gibNummer(){
+      return null;
+  }
+
+  /**  */
+  public String gibName(){
+      return null;
+  }
+
+  /**  */
+  public String gibNachname(){
+      return null;
+  }
+  
+}//Ende Klasse: Telefonbucheintrag
