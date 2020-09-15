3e5bee48ea9ad8b47e165c291b4859684f3829b2
Malte <malte.havers@web.de>
2020-09-15 12:39:54 +0200

    .

--- Telefonbuch.java
+++ Telefonbuch.java
@@ -1,37 +1,63 @@
+import java.util.*;
 /**
 *
 */
 public class Telefonbuch
 {
   /* Attribute */
-    private List a;
-    private BinarySearchTree b;
+    private List<BinarySearchTree> a;
+    private BinarySearchTree/*<>*/ b;
     private boolean sortierung;
+    private Scanner sc;
+  /* Konstruktor */
+  /**
+   *Erstellt die KlaSSe Tefelonbuch. 
+   */
+  public Telefonbuch ()
+  {
+      //
+  }
   /* Methoden */
-  /**  */
-  public void hinzufuegen(String  eintrag){
+  /**
+   * Holt sich Name, Nachname
+   * und Nummer des Hinzuzufügenden
+   * von dem Hinzufüger.
+   */
+  public void hinzufuegen(String  eintrag)
+  {
+      sc = new Scanner(System.in);
+      //
   }
 
   /**  */
-  public void aendereSortierung(){
+  public void aendereSortierung()
+  {
+      //
   }
 
   /**  */
-  public Telefonbucheintrag search(String  eingabe){
+  public Telefonbucheintrag search(String  eingabe)
+  {
+      
       return null;
   }
 
   /**  */
-  public int uebersetzeInIndex(String  eingabe){
+  public int uebersetzeInIndex(String  eingabe)
+  {
       return 0;
   }
 
   /**  */
-  public void löschen(String  eingabe){
+  public void löschen(String  eingabe)
+  {
+      //
   }
 
   /**  */
-  public void veraendern(String  eingabe){
+  public void veraendern(String  eingabe)
+  {
+      //
   }
 
 }//Ende Klasse: Telefonbuch
